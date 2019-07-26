3e53a02508e0a5f155a8795b26a716484b9296ed
wendellcalixtoo <wendellcalixtoo@gmail.com>
2019-07-26 06:59:25 -0300

    App com Hooks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
 
 function App() {
+    const [tech, setTech] = useState([]);
+    const [newTech, setNewTech] = useState('');
+
+    const handleAdd = useCallback(() => {
+        setTech([...tech, newTech]);
+        setNewTech('');
+    }, [newTech, tech]);
+
+    useEffect(() => {
+        const storageTech = localStorage.getItem('tech');
+
+        if (storageTech) {
+            setTech(JSON.parse(storageTech));
+        }
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem('tech', JSON.stringify(tech));
+    }, [tech]);
+
+    const techSize = useMemo(() => tech.length, [tech]);
+
     return (
-        <div className="App">
-            <h1>Hello Redux</h1>
-        </div>
+        <>
+            <ul>
+                {tech.map(t => (
+                    <li key={t}>{t}</li>
+                ))}
+            </ul>
+            <strong>Você tem {techSize} tecnologias</strong>
+            <br />
+            <input value={newTech} onChange={e => setNewTech(e.target.value)} />
+            <button type="button" onClick={handleAdd}>
+                Adicionar
+            </button>
+        </>
     );
 }
 
